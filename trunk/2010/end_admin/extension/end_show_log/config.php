eb85878da7cd39af0e96ea48f5a6037ead592b09
longbill.cn <longbill.cn@5c02aa36-25ec-11df-9030-3bcc40d9c775>
2010-05-02 17:11:11 +0000

    增加一个扩展

---
+++ trunk/2010/end_admin/extension/end_show_log/config.php
@@ -0,0 +1,11 @@
+<?php
+$end_extension['end_show_log'] = array(
+	'name'=>'查看后台操作日志',
+	'icon'=>'icon.png',
+	'content'=>'end_show_log.php',
+	'author'=>'Longbill',
+	'email'=>'longbill.cn@gmail.com',
+	'url'=>'http://www.longbill.cn',
+	'description'=>'查看所有后台操作记录',
+	
+);
