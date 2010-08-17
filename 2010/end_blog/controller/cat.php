ac57719649cb43831bba262a7c3674651d65c0e6
longbill.cn <longbill.cn@5c02aa36-25ec-11df-9030-3bcc40d9c775>
2010-08-17 17:30:09 +0000

    remove end_blog

--- 2010/end_blog/controller/cat.php
+++
@@ -1,8 +0,0 @@
-<?php
-$n = $_GET['argv'][0];
-if (!$n) die('404');
-$cat = model('category')->get_one(array('url'=>$n));
-if (!$cat) die('404');
-$view_data['items'] = end_page(model('blog'),array('status'=>1,'category_id'=>$cat['category_id'],'order'=>'order_id DESC,create_time DESC'),5);
-$view_data['title'] = $cat['name'];
-$_viewer = 'index.html';
